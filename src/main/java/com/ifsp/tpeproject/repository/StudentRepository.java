a0b3b7b92ee4f1c471adb91c2f79fb574a48e35d
Lucas Briz <lucasbriz@outlook.com>
2023-10-29 11:34:17 -0300

    changed project core

--- src/main/java/com/ifsp/tpeproject/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ifsp.tpeproject.repository;
-
-import com.ifsp.tpeproject.entity.Student;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
