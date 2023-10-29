b167c4eecdfab005af3499f8cbda83d06966a0bf
Lucas Briz <lucasbriz@outlook.com>
2023-10-29 12:21:01 -0300

    organizing files and created dockerfile

--- src/main/java/com/ifsp/tpeproject/controller/StudentController.java
+++ src/main/java/com/ifsp/tpeproject/controller/StudentController.java
@@ -3,12 +3,14 @@ package com.ifsp.tpeproject.controller;
 import com.ifsp.tpeproject.entity.Student;
 import com.ifsp.tpeproject.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/student")
 public class StudentController {
